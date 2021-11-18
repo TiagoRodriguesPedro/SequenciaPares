57f0ab65cb8992cada6deed565cbbc11fe169f16
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-18 00:42:05 -0300

    resoluçao exercicio Sequenciapares

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // receba um numero positivo
+            // exibe todos os numeros pares entere  0 e o numero digitado
+            
+        int final;
+
+        int inicio = 0;
+
+           Console.Write("digite um numero");
+           
+          final = Convert.ToInt32(Console.ReadKey());
+
+           while (inicio < final)
+           {
+              if (inicio % 2 == 0){
+                  Console.Write($"{inicio}");
+                  }         
+          
+          inicio = inicio = +1 ;
+         }
+
+        
+        
+
+
+
+        }
+    }
+}
